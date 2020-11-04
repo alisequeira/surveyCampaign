37f6e7a3bbd5a8ae533c2eb8b227f57f3c65dbbf
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-11-04 12:01:01 -0500

    setting up google strategy

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const passport = require('passport');
 const googleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express(); //Generate a new express aplication
 
-passport.use(new googleStrategy());//create a new instance of google oauth
+passport.use(new googleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/calback'
+}, (accesToken) => {
+    console.log(accesToken);
+})
+);//create a new instance of google oauth
 
 const PORT = process.env.PORT || 5000; //If there an environment variable that has been already defined by heroku then used, IFNOT use port 5000
 //so in development environment we're listenig in port 5000 and in producction we're listening in wherever port heroku set to us
