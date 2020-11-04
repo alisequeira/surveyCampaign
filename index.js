7cd99b7c909ca56f26ec37bc14e4bafe6deb4fba
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-11-04 12:31:01 -0500

    testing oauth

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ passport.use(new googleStrategy({
 })
 );//create a new instance of google oauth
 
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email'] //optional object that we passed in scope, scope specify to google servers what kind of access we want to have inside of this
+}));
 const PORT = process.env.PORT || 5000; //If there an environment variable that has been already defined by heroku then used, IFNOT use port 5000
 //so in development environment we're listenig in port 5000 and in producction we're listening in wherever port heroku set to us
 app.listen(PORT, () => console.log('server listenig'));
