0a143f28a69ab4be7c3a9f325c6aa19e15d2d8e3
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-11-04 13:12:12 -0500

    adding nodemon

--- index.js
+++ index.js
@@ -8,14 +8,20 @@ passport.use(new googleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-}, (accesToken) => {
-    console.log(accesToken);
+}, (accesToken, refreshtoken, profile, done) => {//this function will fire once the user has been redirect to callbackURL
+    //this function is our opportunity to now take this identifying user information and save it to our data base
+    console.log('accesToken: ', accesToken);
+    console.log('refresh token: ', refreshtoken);
+    console.log('profile: ', profile);
 })
 );//create a new instance of google oauth
 
 app.get('/auth/google', passport.authenticate('google', {
     scope: ['profile', 'email'] //optional object that we passed in scope, scope specify to google servers what kind of access we want to have inside of this
 }));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 const PORT = process.env.PORT || 5000; //If there an environment variable that has been already defined by heroku then used, IFNOT use port 5000
 //so in development environment we're listenig in port 5000 and in producction we're listening in wherever port heroku set to us
 app.listen(PORT, () => console.log('server listenig'));
