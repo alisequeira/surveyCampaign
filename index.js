192188fd26ab819784af172acec82eec3dc37ce8
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-11-05 11:51:00 -0500

    adding mongoDb into the project

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 require('./services/passport');
 const authRoute = require('./routes/authRoute');
+const mongoose = require('mongoose');
+const keys = require('./config/keys')
 const app = express(); //Generate a new express aplication
 
 authRoute(app);
+mongoose.connect(keys.mongoURI);
 const PORT = process.env.PORT || 5000; //If there an environment variable that has been already defined by heroku then used, IFNOT use port 5000
 //so in development environment we're listenig in port 5000 and in producction we're listening in wherever port heroku set to us
 app.listen(PORT, () => console.log('server listenig'));
