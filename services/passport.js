e71540c954cdc5131cca3f6a2feb23184f5978c1
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-11-05 14:12:59 -0500

    passport callback

--- services/passport.js
+++ services/passport.js
@@ -11,5 +11,16 @@ passport.use(new googleStrategy({
     callbackURL: '/auth/google/callback'
 }, (accesToken, refreshtoken, profile, done) => {//this function will fire once the user has been redirect to callbackURL
     //this function is our opportunity to now take this identifying user information and save it to our data base
-    new User({ googleId: profile.id }).save() //.save() save the instance in mongoDB
+
+    User.findOne({ googleId: profile.id })//return a promise
+        .then((existingUser) => {
+            if (existingUser) {
+                //we already have a record with the given id
+                done(null, existingUser);//done indicate to passport that we are finished and may continue with the workflow
+            } else {
+                //we don't have a user record with this id, make a new record
+                new User({ googleId: profile.id }).save() //.save() save the instance in mongoDB
+                    .then(user => done(null, user));
+            }
+        });
 }));//create a new instance of google oauth
