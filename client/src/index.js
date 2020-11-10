d9662ac09bc3ad8911cf0a0a98059583632a9450
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-11-10 13:02:02 -0500

    creating a redux store and hook it up into the app

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App';
-
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+/*
+--> First argument is all the diference reducer inside my application
+--> Second argument is the initial state of my application, this is most relevant when we are working in server side rendering,
+    by this project we don't really care to much about the initial state so I'm just gonna leave an empty object
+--> Finally apply middleware
+*/
+const store = createStore(() => [], {}, applyMiddleware())
 ReactDom.render(
-    <App />,
+    //create a store a redux store at the very top level of my application
+    //and hook it up in the react application by placing the provider tag
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.querySelector('#root')
 );
