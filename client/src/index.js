17b0f9528b4a686caef41c23db589a7d0216b246
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-11-10 12:42:08 -0500

    client setup installing root modules

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ReactDom from 'react-dom';
+import App from './components/App';
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDom.render(
+    <App />,
+    document.querySelector('#root')
 );
