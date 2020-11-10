830a871cb0606d5f72c7b63f80856ff973128b5f
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-11-10 14:51:42 -0500

    meterializeCSS webpack config

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import Header from './Header';
 import { BrowserRouter, Route } from 'react-router-dom';
 /**
  * BrowserRouter is the thing that tell react router how to behave
  * Route is a react component that setup the component to navigate
  */
 
-const Header = () => <h2>Header</h2>
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
