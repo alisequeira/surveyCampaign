1693892cde1efb9b902b91eda3d328ad9d2c25da
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-11-10 14:31:48 -0500

    route configuration

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+/**
+ * BrowserRouter is the thing that tell react router how to behave
+ * Route is a react component that setup the component to navigate
+ */
+
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
     return (
         <div>
-            Hi there!
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/surveys" component={Dashboard} />
+                    <Route exact path="/surveys/new" component={SurveyNew} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 }
