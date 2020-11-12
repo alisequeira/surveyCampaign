edbca0b3b6c95aaa9739e0b4c8faf9a5522583c2
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-11-12 12:19:23 -0500

    appling redux-thunk

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import ReactDom from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';//redux thunk is to make asyncronous acctions creater behave the way we expect
 import reducers from './reducers';
 /*
 --> First argument is all the diference reducer inside my application
@@ -11,7 +12,7 @@ import reducers from './reducers';
     by this project we don't really care to much about the initial state so I'm just gonna leave an empty object
 --> Finally apply middleware
 */
-const store = createStore(reducers, {}, applyMiddleware())
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 ReactDom.render(
     //create a store a redux store at the very top level of my application
     //and hook it up in the react application by placing the provider tag
