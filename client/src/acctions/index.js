b3ee918ce826d1acdd2cb50be2499a087bf10b3f
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-11-12 12:25:15 -0500

    refactoring App component

--- client/src/acctions/index.js
+++ client/src/acctions/index.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-
+/*
+  v1. whwnerver the action creator is called it will return a 
+  function and it will automatically call it with the dispatch
+    
+    We then make a request. We wait until we get the response back from our API
+    And then once we have the reponse only at that point in time will we actually dispatch our action. 
+  */
 const fetchUser = () => {
     return function (dispatch) {
         axios.get('/api/current_user')
