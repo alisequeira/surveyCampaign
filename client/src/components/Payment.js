d970ec99f4f09221053e23a79b19b639a66b8951
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-11-13 13:01:00 -0500

    The Payment Component

---
+++ client/src/components/Payment.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+class Payment extends React.Component {
+    render() {
+        debugger
+        return (
+            <StripeCheckout
+                amount={500}
+                token={token => console.log(token)}
+                stripeKey={process.env.REACT_APP_STRIPE_KEY}
+            />
+        );
+    }
+}
+//the amount prop is means to be cents 500 cents => 5 dlrs
+export default Payment;
