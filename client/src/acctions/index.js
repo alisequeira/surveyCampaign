e933b668c5fcd590ca91afe8ab41ea41afc3b3e6
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-11-13 14:18:20 -0500

    Reusing Action Types (handleToken action creator)

--- client/src/acctions/index.js
+++ client/src/acctions/index.js
@@ -16,6 +16,12 @@ import { FETCH_USER } from './types';
     };
   */
 export const fetchUser = () => async dispatch => {
-    const res = await axios.get('/api/current_user');
-    dispatch({ type: FETCH_USER, payload: res.data });
+  const res = await axios.get('/api/current_user');
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+export const handleToken = (token) => async dispatch => {
+  const res = await axios.post('/api/stipe', token);
+
+  dispatch({ type: FETCH_USER, payload: res.data });
+}
