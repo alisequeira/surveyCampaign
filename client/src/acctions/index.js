93e4a7777d70d9237de6a33016fbfa8343da7ac0
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-11-14 18:40:42 -0500

    Finalizing a charge

--- client/src/acctions/index.js
+++ client/src/acctions/index.js
@@ -21,7 +21,7 @@ export const fetchUser = () => async dispatch => {
 };
 
 export const handleToken = (token) => async dispatch => {
-  const res = await axios.post('/api/stipe', token);
+  const res = await axios.post('/api/stripe', token);
 
   dispatch({ type: FETCH_USER, payload: res.data });
 }
