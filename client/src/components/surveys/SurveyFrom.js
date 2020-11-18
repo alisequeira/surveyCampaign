f65dd604347f7612fa839f4af1fdfe0de20f03c2
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-11-18 12:15:25 -0500

    Redux Form in practice

--- client/src/components/surveys/SurveyFrom.js
+++ client/src/components/surveys/SurveyFrom.js
@@ -1,13 +1,22 @@
 //SurveyForm show a form for a user to add input
 import React from 'react';
-import { reduxForm } from 'redux-form';
-
+import { reduxForm, Field } from 'redux-form';
+/**
+ * The Field component is a helper provided by redux form
+ * rendering absolutely any type of treditional HTML form element
+ * (text areas, text inputs, file inputs, checkboxes, radio buttons..etc)
+ * 
+ * HandleSubmit is a function provided by reduxForm helper that we wireUp at the bottom
+ */
 class SurveyForm extends React.Component {
 
     render() {
         return (
             <div>
-                SurveyForm
+                <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+                    <Field type="text" name="surveyTitle" component="input" />
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         )
     }
