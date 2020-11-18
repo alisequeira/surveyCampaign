7dd667251a5ca07f84e4dcda249ee0eaca43c071
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-11-18 16:25:24 -0500

    advancing Form SurveyForm

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,13 +1,27 @@
 //SurveyNew show SurveyForm and SurveyFormReview
 import React from 'react';
 import SurveyForm from './SurveyFrom';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { showFormReview: false };
+    }
+
+    renderContent = () => {
+        if (this.state.showFormReview) {
+            return <SurveyFormReview />
+        }
+
+        return <SurveyForm onSurveySubmit={() => this.setState({ showFormReview: true })} />
+    }
 
     render() {
         return (
             <div>
-                <SurveyForm />
+                {this.renderContent()}
             </div>
         )
     }
