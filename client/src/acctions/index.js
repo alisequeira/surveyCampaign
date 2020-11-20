b6c7ab8ddf16a6d10fdc8aac0edd859c453ac540
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-11-20 13:08:39 -0500

    Wiring Up to Redux

--- client/src/acctions/index.js
+++ client/src/acctions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 /*
   v1. whwnerver the action creator is called it will return a 
@@ -30,4 +30,9 @@ export const submitSurvey = (values, history) => async (dispatch) => {
   const res = await axios.post('/api/surveys', values);
   history.push('/surveys')
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSuveys = () => async dispatch => {
+  const res = await axios.get('/api/surveys');
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
