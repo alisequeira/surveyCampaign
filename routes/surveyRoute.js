96cef9d9f85ff08ed21d8cd763fd6ce48d6c2bda
Ali Sequeira <alisequeira.la.240@gmail.com>
2020-11-20 11:58:55 -0500

    Odds Ends around surveys

--- routes/surveyRoute.js
+++ routes/surveyRoute.js
@@ -15,19 +15,33 @@ const Survey = mongoose.model('surveys');
 
 
 module.exports = (app) => {
-    app.get('/api/surveys/thanks', (req, res) => {
+    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
         res.send('THANKS FOR VOTING!');
     });
 
     app.post('/api/surveys/webhooks', (req, res) => {
-        const events = _.map(req.body, ({ email, url }) => {
-            const pathname = new URL(url).pathname;
-            const p = new Path('/api/surveys/:surveyId/:choice');
-            const match = p.test(pathname);
-            if (match) return { email, surveyId: match.surveyId, choice: match.choice };
-        });
-        const compacEvents = _.compact(events);
-        const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+        const p = new Path('/api/surveys/:surveyId/:choice');
+        _.chain(req.body)
+            .map(({ email, url }) => {
+                const match = p.test(new URL(url).pathname);
+                if (match) return { email, surveyId: match.surveyId, choice: match.choice };
+            })
+            .compact()
+            .uniqBy('email', 'surveyId')
+            .each(({ surveyId, email, choice }) => {
+                Survey.updateOne({
+                    id: surveyId,
+                    recipients: {
+                        $elemMatch: { email: email, response: false }
+                    }
+                }, {
+                    $inc: { [choice]: 1 },
+                    $set: { 'recipients.$.responded': true },
+                    lastResponded: new Date()
+                }).exec();
+            })
+            .value()
+
 
         res.send({});
     });
